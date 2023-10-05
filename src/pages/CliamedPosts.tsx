09d4b82733deaa8dc718070f0437cde9f3b98e8b
Akshay Prakash <rashsh2000sh@gmail.com>
2023-10-06 02:56:14 +0530

    Deploy

--- src/pages/CliamedPosts.tsx
+++ src/pages/CliamedPosts.tsx
@@ -29,10 +29,6 @@ function ClaimedPosts() {
             
             <div className='flex flex-col items-center w-full h-full gap-4 overflow-scroll'>
             <h1 className='font-black text-[48px] text-red-500 mb-7 w-[840px] font-Satoshi16px text-left'>
-            {/* <div className="flex flex-row">
-                        <Heading as="h1" level="1" color={"black"}>{"Your"}</Heading>
-                        <Heading as="h1" level="1" color={"green"}>{" Claimed Posts"}</Heading>
-              </div> */}
                 <span className='text-black-100 font-normal'>Your</span> Claimed Posts
             </h1>
             {
